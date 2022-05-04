7b2759befc50d15574e0422d5add8aebd1ad7d72
Aluízio Catão <aluizio2210@gmail.com>
2022-05-04 18:22:30 -0500

    Second Commit

---
+++ src/assets/js/script.js
@@ -0,0 +1,22 @@
+function mudaTema(temaSelecionado) {
+    let url = "estilo-tema-" + temaSelecionado + ".css";
+    let linkTema = document.querySelector("#link-tema");
+    linkTema.href = url;
+}
+
+window.onload = () => {
+
+    let tema = localStorage.getItem("tema");
+    if (tema) {
+        mudaTema(tema);
+    }
+
+    let selectTema = document.querySelector("select#tema");
+    selectTema.addEventListener("change", evento => {
+        let temaSelecionado = evento.target.value;
+        if (temaSelecionado) {
+            mudaTema(temaSelecionado);
+            localStorage.setItem("tema", temaSelecionado);
+        }
+    });
+}
